b3d61b834b93fdf90ec2c2504ccad670ed96588c
Reitumetse <joycreamoeletsi@gmail.com>
2022-09-27 13:28:05 +0200

    update

---
+++ 0x0F-load_balancer/2-puppet_custom_http_reponse_header.pp
@@ -0,0 +1,23 @@
+# install and config the nginx using puppet
+exec { 'update':
+  command  => 'sudo apt-get update',
+  provider => shell,
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['update'],
+}
+
+file_line { 'headercustom':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => ':80 default_server;',
+  line    => "add_header X-Served-By ${hostname};",
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => File_line['headercustom'],
+}
