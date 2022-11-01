7cad28afeb5e98b3b649db1d6023dd4e21ce7b1f
Reitumetse <joycreamoeletsi@gmail.com>
2022-11-02 01:54:38 +0200

    recurse

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+return top 10 hot posts of a given subreddit
+"""
+import requests
+import sys
+after = None
+
+
+def recurse(subreddit, hot_list=[]):
+    """ The recurse function"""
+    global after
+    headers = {'User-Agent': 'Hello'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
+
+    if response.status_code == 200:
+        next_ = response.json().get('data').get('after')
+        if next_ is not None:
+            after = next_
+            recurse(subreddit, hot_list)
+        list_titles = response.json().get('data').get('children')
+        for title_ in list_titles:
+            hot_list.append(title_.get('data').get('title'))
+        return hot_list
+    else:
+        return (None)
